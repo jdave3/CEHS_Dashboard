f097b2a2d296508fedeed530944cd0b475eb0612
jdave3 <jdave3@uic.edu>
2015-11-08 16:06:20 -0600

    Code with Help Menu- 11/8

--- Code/js/src/script.js
+++ Code/js/src/script.js
@@ -1,28 +1,7 @@
 $(document).ready(function(){
 	
-	$('#email').accordion({collapsible: true, active: false});
+	$('#About').accordion({collapsible: true, active: false});
 	
-	$('#contact').accordion({collapsible: true, active: false});
-	
-	$('#skillslink').mouseenter(function(){
-		$(this).addClass('zoom');
-	});
-	$('#skillslink').mouseleave(function(){
-		$(this).removeClass('zoom');
-	});
-	
-	$('#contactlink').mouseenter(function(){
-		$(this).addClass('zoom');
-	});
-	$('#contactlink').mouseleave(function(){
-		$(this).removeClass('zoom');
-	});
-	
-	$('#maplink').mouseenter(function(){
-		$(this).addClass('zoom');
-	});
-	$('#maplink').mouseleave(function(){
-		$(this).removeClass('zoom');
-	});
+	$('#use').accordion({collapsible: true, active: false});
 
 });
--- Code/js/src/statemap.js
+++ Code/js/src/statemap.js
@@ -1,29 +1,32 @@
+var global_nested_statewise=[];
+
 function d3Tutorial(){
-    
+	
  	//Assign colors
 	var mapColor = "#D2D2D2";
 	var mapText = "#000082";
 	var mapBorder = "#464646";
 	var mapMarker = "#820000";
-	sizeChange();
+	//sizeChange();
 	//projecting 3D to 2D and setting size of map
 	d3.select(window)
     		.on("resize", sizeChange);
-			
-	d3.select(window)
-    		.on("load", sizeChange);
-			
+	document.body.onload = function() {
+			sizeChange();
+	}		
 	
+	var tip =  d3.select("#map")
+				.append("div")  
+				.attr("class", "tooltip")
+				.attr("id","tooltip")
+				.style("opacity", 0);
 	//https://milkator.wordpress.com/2013/02/25/making-a-map-of-germany-with-topojson/
 	
 	var projection = d3.geo.albersUsa()
-					   .scale([900]);
+					   .scale(1600);
 	
-	function sizeChange() {
-	    d3.select("g").attr("transform", "scale(" + $("#map").width()/900 + ")");
-	    $("svg").height($("#map").width()*0.4);
-		console.log("called");
-	}
+
+	var init_height = $("#map").width()*0.3;
 
 	
 	//Define path
@@ -33,54 +36,76 @@ function d3Tutorial(){
 				 
 	//Define Map holder
 	//https://milkator.wordpress.com/2013/02/25/making-a-map-of-germany-with-topojson/
-	var svg = d3.select("#map").append("svg")
+	var svg = d3.select("#map")	
+								.append("div")
+								.append("svg")
 								.attr("id","mapHolder")
 								.attr("width", "100%") 
-								.attr("height", "300px") 
-								.append("g");
+								.attr("x","10")
+								//.attr("height", init_height)
+								//.attr("transform","scale($("#map").width()/900)")
+								//.style("m.append("g");argin-left","50%")
+								
 	
-	var color = d3.scale.quantize()
-                    .range(["rgb(237,248,233)", "rgb(186,228,179)",
-                     "rgb(116,196,118)", "rgb(49,163,84)","rgb(0,109,44)"]);
-					 
-	d3.csv("CEHS_Dashboard_data_table_test.csv", function(data) {
+
+	var color_range = ["#f7fcfd","#ccece6","#99d8c9","#66c2a4","#41ae76","#006d2c","#00441b"]	
+	d3.csv("CEHS_Dashboard_data_table.csv", function(data) {
 			
 	//variable for citywise
 	var nested_statewise = d3.nest()
-						.key(function(d){return JSON.stringify($.trim(d.State))})
+						.key(function(d)
+						{return d.State.trim()})
 						.rollup(function(d) { 
-							return d3.sum(d, function(g) {return g.Indemnity_Cost; });
+							/*return {d3.sum(d, function(g) {return g.Indemnity_Cost; })/d.length,
+							d.length}*/
+							return  {
+								total_count: d.length,
+								litigated_count: d.filter(function(d){return d.Legal_Indicator.trim() == "Y"}).length,
+								nonlitigated_count: d.filter(function(d){return d.Legal_Indicator.trim() == "N"}).length,			
+								total_Indemnity_Cost: d3.sum(d, function(d) { return d.Indemnity_Cost; }).toFixed(2),
+								total_Medical_Cost: d3.sum(d, function(d) { return d.Medical_Cost; }).toFixed(2),
+								total_Expense_Cost: d3.sum(d, function(d) { return d.Expense_Cost; }).toFixed(2)
+							};
 							})
 						.entries(data);	
-	console.log(nested_statewise);
-	
-	color.domain([
-                d3.min(data, function(d) { return d.value; }),
-                d3.max(data, function(d) { return d.value; })
-        ]);
+
+	nested_statewise = nested_statewise.sort(function(obj1, obj2) {
+						return obj1.values.total_Medical_Cost - obj2.values.total_Medical_Cost;
+						});
+	for(var i=0;i<nested_statewise.length;i++){
+					global_nested_statewise.push(
+					{key: nested_statewise[i].key,
+					total_count: nested_statewise[i].values.total_count,
+					litigated_count: nested_statewise[i].values.litigated_count,
+					nonlitigated_count: nested_statewise[i].values.nonlitigated_count,
+					total_Indemnity_Cost: nested_statewise[i].values.total_Indemnity_Cost,
+					total_Medical_Cost: nested_statewise[i].values.total_Medical_Cost,
+					total_Expense_Cost: nested_statewise[i].values.total_Expense_Cost
+					});
+		
+			}
+	console.log("global_nested_statewise:"+global_nested_statewise[0].total_count);	
 	
 	
 			
 	//loading the json file
 	d3.json("midwest-states.json", function(json) {
-	    for (var i = 0; i < data.length; i++) {
+	    for (var i = 0; i < nested_statewise.length; i++) {
 
             //Grab state name
-            var dataState = data[i].State;
+            var dataState = nested_statewise[i].key;
 
             //Grab data value, and convert from string to float
-            var dataValue = parseFloat(data[i].value);
+            var dataValue = parseFloat(nested_statewise[i].values);
 
             //Find the corresponding state inside the GeoJSON
             for (var j = 0; j < json.features.length; j++) {
-
             var jsonState = json.features[j].properties.name;
-
             if (dataState == jsonState) {
 
                 //Copy the data value into the JSON
                 json.features[j].properties.value = dataValue;
-
+				json.features[j].properties.rank = i;
                 //Stop looking through the JSON
                 break;
 				}
@@ -96,19 +121,64 @@ function d3Tutorial(){
 		.attr("stroke",mapBorder)
 		.style("fill", function(d) {
                     //Get data value
-                    var value = d.properties.value;
-
-                    if (value) {
-                            //If value exists…
-                            return color(value);
-                    } else {
-                            //If value is undefined…
-                            return "#ccc";
-                    }
+                    var rank = d.properties.rank;
+					console.log("value:"+rank);
+					return color_range[rank];
+                    
         })
 		
 		
-
+		
+		.on('mouseover',function(d,i){
+		d3.select(".tooltip").style("display","block");
+		var currentState = d.properties.name;
+		console.log(currentState);
+		d3.select(this).style('fill-opacity', 1.0);										
+		tip.transition()
+		   .style("opacity",1.00);
+		for (var j = 0; j < nested_statewise.length; j++) {
+				if(nested_statewise[j].key == currentState)
+				{
+					/*
+					tip.html("<strong>"+nested_statewise[j].key
+					+"</strong><br>Total_Claims:"+nested_statewise[j].values.total_count
+					+"<br>Litigated Cases:"+nested_statewise[j].values.litigated_count
+					+"<br>Non-Litigated Cases:"+nested_statewise[j].values.nonlitigated_count
+					+"<br>Total_Indemnity_Cost:"+nested_statewise[j].values.total_Indemnity_Cost
+					+"<br>Total_Medical_Cost:"+nested_statewise[j].values.total_Medical_Cost
+					+"<br>Total_Expense_Cost:"+nested_statewise[j].values.total_Expense_Cost
+					);*/
+					tip.html("<table id = 'tooltiptable' border='2'><tr><th colspan='2' style>"+nested_statewise[j].key+"</th></tr>"+
+					"<tr><td>Total_Claims:</td>"+
+					"<td>"+nested_statewise[j].values.total_count+"</td></tr>"+
+					"<tr><td>Litigated Cases:</td>"+
+					"<td>"+nested_statewise[j].values.litigated_count+"</td></tr>"+
+					"<tr><td>Non-Litigated Cases</td>"+
+					"<td>"+nested_statewise[j].values.nonlitigated_count+"</td></tr>"+
+					"<tr><td>Total_Indemnity_Cost:</td>"+
+					"<td>"+nested_statewise[j].values.total_Indemnity_Cost+"</td></tr>"+
+					"<tr><td>Total_Medical_Cost:</td>"+
+					"<td>"+nested_statewise[j].values.total_Medical_Cost+"</td></tr>"+
+					"<tr><td>Total_Expense_Cost:</td>"+
+					"<td>"+nested_statewise[j].values.total_Expense_Cost+"</td></tr>"+
+					"</table>"
+					);
+					
+					break;
+				}
+		}
+		/*tip.html("<strong>"+nested_statewise[0].key+", "+nested_statewise[0].values+": "+"</strong><span style='color: white'>"+ nested_statewise[0].values.length+"</span>") 
+		*/
+		
+			tip.style("left", (d3.event.pageX) + "px")
+			.style("color","white")			
+			.style("top", (d3.event.pageY - 25) + "px");	
+		})
+		
+		
+		.on('mouseout',function(d,i){
+		d3.select(".tooltip").style("display","none");
+		})
 		
 		svg.selectAll("text")
 		.data(json.features)
@@ -128,6 +198,53 @@ function d3Tutorial(){
 		.attr('font-size','8pt')
 		.attr("font-family","Times New Roman")
 		.attr("font-weight","bold");
+	});	
 	});
+	
+    sizeChange();
+}
+function sizeChange() {
+	    d3.select("g").attr("transform", "scale(" + $("#map").width()/900 + ") translate(" + $("#map").width()*-1/4+")");
+		//d3.select("g").attr("transform","translate(" + $("#map").width()/40+","+$("#map").height()/30+")");
+	    $("svg").height($("#map").width()*0.3);
+		console.log($("#map").width()*-1/40);
+		console.log("called");
+}
+
+function makeTableHTML() {
+	var tab = d3.select("#map")	
+					.append("div")
+					.attr("id","tableHolder")
+					.style("float","right");
+	
+    var result = "<table id = 'statetable' border=1 class='table table-bordered  table-hover mytableclass'>";
+		result += "<tr style = 'background-color:#EDECE9'>";
+		result += "<th>Rank</th>";
+		result += "<th>State</th>";
+		result += "<th>Total Cases</th>";
+		result += "<th>Litigated Cases</th>";
+		result += "<th>Non-Litigated Cases</th>";
+		result += "<th>Total Indemnity Cost</th>";
+		result += "<th>Total Medical Cost</th>";
+		result += "<th>Total Expense Cost</th>";		
+        result += "</tr>";
+    for(var i=0; i<global_nested_statewise.length; i++) {
+        result += "<tr>";
+		result += "<td>"+(i+1)+"</td>";
+        result += "<td>"+global_nested_statewise[i].key+"</td>";
+		result += "<td>"+global_nested_statewise[i].total_count+"</td>";
+		result += "<td>"+global_nested_statewise[i].litigated_count+"</td>";
+		result += "<td>"+global_nested_statewise[i].nonlitigated_count+"</td>";
+		result += "<td>"+global_nested_statewise[i].total_Indemnity_Cost+"</td>";
+		result += "<td>"+global_nested_statewise[i].total_Medical_Cost+"</td>";
+		result += "<td>"+global_nested_statewise[i].total_Expense_Cost+"</td>";		
+        result += "</tr>";
+    }
+    result += "</table>";
+	$("#statetable").remove();
+	tab.html(result);
+	
+	$("button").click(function(){
+    $("table").toggle();
 	});
 }
